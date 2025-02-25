fe343d2635614a7ac46bacf2e42173a8dd46a1a8
Richard Weyfung Cheng <richardwcheng@Richard-Mac-mini.local>
2025-02-25 13:09:44 -0500

    Initial commit with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Comparing the impact of 2018 Tariffs between US and Canadian goods and services using differences in differences and LLMs',
+    author='MADS Lions',
+    license='',
+)
