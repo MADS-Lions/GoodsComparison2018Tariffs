8fbbb5f51133826e70e6fc3d2d9f7c2190df4ae3
Richard Weyfung Cheng <richardwcheng@Richard-Mac-mini.local>
2025-02-26 11:49:59 -0500

    Update visualize.py with more functions

---
+++ src/visualization/visualize.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+from scipy.signal import correlate
+
+def line_plot(data, x, y, y2, color=None, title=""):
+    chart1 = alt.Chart(data).mark_line().encode(
+        x=x,
+        y=y,
+        color=color
+    ).properties(
+        title=title
+    )
+    chart2 = alt.Chart(data).mark_line().encode(
+        x=x,
+        y=y2,
+        color=color
+    )
+    return chart1 + chart2
+
+
+def histogram(data, x, color=None, title=""):
+    chart = alt.Chart(data).mark_bar().encode(
+        x=alt.X(x, bin=True),
+        y='count()',
+        color=color
+    ).properties(
+        title=title
+    )
+    return chart
+
+def cross_correlation_plot(data1, data2, name_data1, name_data2):
+    correlated_data = correlate(data1, data2)
+    lags = np.arange(-len(data1) + 1, len(data1))
+    plt.figure(figsize=(10, 5))
+    plt.stem(lags, correlated_data, basefmt=" ")
+    plt.title('Cross-Correlation Between Two Time Series')
+    plt.xlabel('Lag')
+    plt.ylabel('Correlation Coefficient')
+    plt.grid()
+    plt.legend()
+    plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.plot(range(len(data1)), data1, label=name_data1)
+    plt.plot(range(len(data2)), data2, label=name_data2)
+    plt.title("Cross-correlation Plot for: " + name_data1 + ' and ' + name_data2)
+    plt.show()
+    return None
