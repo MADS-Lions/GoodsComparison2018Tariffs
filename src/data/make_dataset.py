15ed8e0fbd020795d714dc08709cecb995cfdcd6
Richard Weyfung Cheng <richardwcheng@Richard-Mac-mini.local>
2025-02-25 19:12:15 -0500

    Changes to processing the data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,7 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 
 
 @click.command()
@@ -14,6 +15,35 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    Canadian_CPI_dataset = load_data("../../data/Canadian_CPI.csv")
+    USA_CPI_dataset = load_data("../../data/USA_CPI.csv")
+    Canadian_CPI_dataset = manipulate_data(Canadian_CPI_dataset, "Canadian")
+    USA_CPI_dataset = manipulate_data(USA_CPI_dataset, "USA")
+    save_data(Canadian_CPI_dataset, "../../data/processed/Canadian_CPI_Processed.csv")
+    save_data(USA_CPI_dataset, "../../data/processed/USA_CPI_Processed.csv")
+    logger.info('Data processing completed successfully')
+def load_data(input_filepath):
+    """Load data from input_filepath"""
+    data = pd.read_csv(input_filepath)
+    return data
+
+def manipulate_data(data, which_data_file):
+    if which_data_file == "Canadian":
+        Canadian_CPI_dataset = data.copy()
+        Canadian_CPI_dataset = Canadian_CPI_dataset.groupby(['Products and product groups', 'REF_DATE']).mean()    
+        Canadian_CPI_dataset.reset_index(inplace=True)
+        return Canadian_CPI_dataset
+    else:
+        USA_CPI_dataset = data.copy()
+        USA_CPI_dataset = USA_CPI_dataset[USA_CPI_dataset['DATA TYPE'] == 'SEASONALLY ADJUSTED INDEX']
+        USA_CPI_dataset.drop(columns = ['ITEM', 'series id', 'DATA TYPE'], inplace=True)
+        USA_CPI_dataset = USA_CPI_dataset.groupby(['TITLE', 'YEAR']).mean()
+        return USA_CPI_dataset
+
+
+def save_data(data, output_filepath):
+    data.to_csv(output_filepath, index=False)
+    return None
 
 
 if __name__ == '__main__':
@@ -28,3 +58,4 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
 
     main()
+    
