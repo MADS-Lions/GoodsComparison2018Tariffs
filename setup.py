35f0573f1c59c2d6cf0c6cc1603544c1740f39e0
Richard Weyfung Cheng <richardwcheng@Richard-Mac-mini.local>
2025-04-02 15:11:16 -0400

    Add setup changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Comparing the impact of 2018 Tariffs between US and Canadian goods and services using differences in differences and LLMs',
+    description='Comparing the impact of 2018 Tariffs between US and Canadian goods and services using ARIMA, Regression Discontinuity, and Visuals',
     author='MADS Lions',
     license='',
 )
