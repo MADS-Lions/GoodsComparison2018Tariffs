6001979c86832d4a7ca88400414a8bf92b262bc0
Richard Weyfung Cheng <richardwcheng@Richard-Mac-mini.local>
2025-04-11 20:33:01 -0400

    Change streamlit for public server use

--- notebooks/Streamlit - visualization_exploration.py
+++ notebooks/Streamlit - visualization_exploration.py
@@ -269,14 +269,14 @@ def plot_individual_product(df, category, date1, date2):
     return chart
 
 
-American_supply_demand = pd.read_csv('./data/processed/USA_Sales_Processed_Final.csv')
-Canadian_supply_demand = pd.read_csv('./data/processed/Canada_Sales_Processed_Final.csv')
-American_df = pd.read_csv('./data/processed/USA_CPI_Processed_2018_2019.csv')
+American_supply_demand = pd.read_csv('../data/processed/USA_Sales_Processed_Final.csv')
+Canadian_supply_demand = pd.read_csv('../data/processed/Canada_Sales_Processed_Final.csv')
+American_df = pd.read_csv('../data/processed/USA_CPI_Processed_2018_2019.csv')
 
 
-Canadian_df = pd.read_csv('./data/processed/Canada_CPI_Processed_2018_2019.csv')
-df_model_data_CAN = pd.read_csv('./data/processed/CAN_Categorized_Products_and_Services_NEW.csv')
-df_model_data_USA = pd.read_csv('./data/processed/US_Categorized_Products_and_Services_NEW.csv')
+Canadian_df = pd.read_csv('../data/processed/Canada_CPI_Processed_2018_2019.csv')
+df_model_data_CAN = pd.read_csv('../data/processed/CAN_Categorized_Products_and_Services_NEW.csv')
+df_model_data_USA = pd.read_csv('../data/processed/US_Categorized_Products_and_Services_NEW.csv')
 dict_CAN = df_model_data_CAN.drop_duplicates(subset=['Product_Service']).set_index('Product_Service')['Category'].to_dict()
 dict_USA = df_model_data_USA.drop_duplicates(subset=['Product_Service']).set_index('Product_Service')['Category'].to_dict()
 
@@ -354,7 +354,7 @@ st.altair_chart(chart6, use_container_width=True)
 
 
 
-US_sales_groceries_clothing = pd.read_csv("./data/raw/ClothingGroceriesUSSalesData.csv")
+US_sales_groceries_clothing = pd.read_csv("../data/raw/ClothingGroceriesUSSalesData.csv")
 US_sales_groceries_clothing['Clothing Sales'] = US_sales_groceries_clothing['Clothing Sales'].str.replace(',', '')  
 US_sales_groceries_clothing['Grocery Sales'] = US_sales_groceries_clothing['Grocery Sales'].str.replace(',', '')
 US_sales_groceries_clothing['Gas Sales'] = US_sales_groceries_clothing['Gas Sales'].str.replace(',', '')
